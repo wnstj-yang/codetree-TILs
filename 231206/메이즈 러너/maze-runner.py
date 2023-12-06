759c0748fcbc15e864c064bb129b4d07812091d7
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-06 23:19:34 +0900

    Title: 메이즈 러너, Time: 226ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 231206/메이즈 러너/maze-runner.py
+++ 231206/메이즈 러너/maze-runner.py
@@ -1,117 +1,122 @@
-# 2023-10-12(목)
-# 풀이 시간 : 21:18 ~ 23:31
-# 첫 제출 : 226ms / 메모리 26MB
+# 2023-12-05
+# 21:11 ~
+
+from collections import deque
 
 
-def move_players():
+def move():
     global total
 
     moved = [[[] for _ in range(N)] for _ in range(N)]
-    for r in range(N):
-        for c in range(N):
-            if len(players[r][c]) > 0:
-                dist = abs(r - ex) + abs(c - ey) # 최단거리
-                for p in players[r][c]:
-                    isMoved = False
+
+    # 최단 거리를 구하고 4방향 움직였을 때 이거 보다 작다면 움직이기 가능
+    for i in range(N):
+        for j in range(N):
+            # 플레이어가 존재하면 움직여준다
+            if len(players[i][j]) > 0:
+                min_dist = abs(i - ex) + abs(j - ey)
+                for p in players[i][j]:
+                    is_moved = False
                     for k in range(4):
-                        nr = r + dx[k]
-                        nc = c + dy[k]
-                        if nr < 0 or nr >= N or nc < 0 or nc >= N:
+                        nx = i + dx[k]
+                        ny = j + dy[k]
+                        if nx < 0 or nx >= N or ny < 0 or ny >= N:
                             continue
-
-                        check_dist = abs(nr - ex) + abs(nc - ey)
-                        if check_dist == 0: # 도착함
-                            total += 1 # 이동 거리 증가
-                            isMoved = True # 탈출한 것이므로 움직였다는 표시
+                        dist = abs(nx - ex) + abs(ny - ey)
+                        # 출구 나가면 끝. 즉, 진또배기 최단거리임
+                        if dist == 0:
+                            total += 1
+                            is_moved = True
+                            check_player[p] = 0
+                            break
+                        # 빈 공간에 최단 거리가 가깝다면 우선순위에 따라 발견되면 움직인다.
+                        # 2개 이상이 아니더라도 거리가 최단거리보다 작으면 4방향 중 가장 가깝다는 의미이기에 조건문에 포함되어 판단한다
+                        if board[nx][ny] == 0 and dist < min_dist:
+                            moved[nx][ny].append(p)
+                            total += 1
+                            is_moved = True
                             break
 
-                        # 참가자가 움직일 수 있고 상하좌우 우선순위에 따라서 움직였을 때의 최단거리가 더 작다면 이동한다
-                        if board[nr][nc] == 0 and check_dist < dist:
-                            moved[nr][nc].append(p + 1) # 이동
-                            total += 1 # 이동 거리 증가
-                            isMoved = True
-                            break
-
-                    if not isMoved:
-                        moved[r][c].append(p)
+                    if not is_moved:
+                        moved[i][j].append(p)
     return moved
 
 
-# 최소 정사각형을 행, 열이 작은 순서로 진행하기 때문에 크기와 조건에 맞게 구했다면 빠져나간다.
 def get_square():
     for l in range(2, N):
-        for x in range(N - l + 1):
-            for y in range(N - l + 1):
-                isExit = False
-                isPlayer = False
-                for i in range(x, x + l):
-                    for j in range(y, y + l):
-                        if len(players[i][j]) > 0:
-                            isPlayer = True
-                        if i == ex and j == ey:
-                            isExit = True
-                        if isPlayer and isExit:
-                            return x, y, l
+        for i in range(N - l - 1):
+            for j in range(N - l - 1):
+                # 여기서 문제 한 번 더 읽고 코드 작성 ! + 회전 시 로직 추가
+                is_people = False
+                is_exit = False
+                for x in range(i, i + l):
+                    for y in range(j, j + l):
+                        if len(players[x][y]) > 0:
+                            is_people = True
+                        if x == ex and y == ey:
+                            is_exit = True
+                        if is_people and is_exit:
+                            return [l, i, j]
+    return [0, 0, 0]
 
-    return 0, 0, 0 # 없으면 0, 0, 0
 
-
-def rotate():
+def rotate(length, x, y):
     global ex, ey
-    sx, sy, length = get_square()
-    if sx == 0 and sy == 0 and length == 0:
-        return
-    temp1 = [[0] * length for _ in range(length)] # 격자판 회전
-    temp2 = [[[] for _ in range(length)] for _ in range(length)] # 참가자 회전
-    rotated1 = [[0] * length for _ in range(length)] # 격자판 회전
-    rotated2 = [[[] for _ in range(length)] for _ in range(length)] # 참가자 회전
+    # 일부분에 있는 거를 회전 시켜야한다.
+    # 탈출구 갱신
+    mid_board = [[0] * N for _ in range(N)]
+    next_board = [[0] * N for _ in range(N)]
+    mid_players = [[[] for _ in range(N)] for _ in range(N)]
+    next_players = [[[] for _ in range(N)] for _ in range(N)]
+    # 1. 값 회전을 수월하게 하기 위해 정사각형에 있는 회전 값들의 시작을 0,0 으로 옮긴다.
+    for i in range(x, x + length):
+        for j in range(y, y + length):
+            mid_board[i - x][j - y] = board[i][j]
+            mid_players[i - x][j - y] = players[i][j]
 
-    # 1. 각각 받은 중간의 정사각형 좌표와 크기를 0,0부터 시작해서 값을 넣는다.
-    for i in range(sx, sx + length):
-        for j in range(sy, sy + length):
-            temp1[i - sx][j - sy] = board[i][j]
-            temp2[i - sx][j - sy] = players[i][j]
-
-    # 2. 만들어진 temp리스트들에다가 90도 시계방향 회전 진행
+    # 2. 0,0에서부터 시작하여 회전을 진행한다.
     for i in range(length):
         for j in range(length):
-            rotated1[j][length - 1 - i] = temp1[i][j]
-            rotated2[j][length - 1 - i] = temp2[i][j]
+            next_board[j][length - 1 - i] = mid_board[i][j]
+            next_players[j][length - 1 - i] = mid_players[i][j]
 
-    # 3. 회전된 것을 격자판, 참가들의 배열에 초기화시킨다.
-    for i in range(sx, sx + length):
-        for j in range(sy, sy + length):
-            board[i][j] = rotated1[i - sx][j - sy]
-            players[i][j] = rotated2[i - sx][j - sy]
-            # 10은 탈출구를 뜻하며 내구도를 깎지 않고 좌표를 갱신시켜준다.
+    # 3. 기존 격자판, 참가자들의 값에 넣어준다 + 벽 내구도 줄이기
+    for i in range(x, x + length):
+        for j in range(y, y + length):
+            board[i][j] = next_board[i - x][j - y]
+            players[i][j] = next_players[i - x][j - y]
             if board[i][j] == 10:
-                ex, ey = i, j
+                ex = i
+                ey = j
                 continue
             board[i][j] -= 1
-            if board[i][j] < 0:
+            if board[i][j] <= 0:
                 board[i][j] = 0
 
 
-N, M, K = map(int, input().split())
-ex, ey = -1, -1
 # 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+N, M, K = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 players = [[[] for _ in range(N)] for _ in range(N)]
+check_player = {}
 total = 0
 for i in range(1, M + 1):
     x, y = map(int, input().split())
-    # 번호와 이동거리
-    players[x - 1][y - 1].append(0)
+    players[x - 1][y - 1].append(i)
+    check_player[i] = 1
 ex, ey = map(int, input().split())
 ex -= 1
 ey -= 1
 board[ex][ey] = 10 # 탈출구
-
-for _ in range(K):
-    players = move_players()
-    rotate()
-
+for i in range(K):
+    if sum(check_player.values()) == 0:
+        break
+    players = move()
+    l, x, y = get_square()
+    if l + x + y == 0:
+        continue
+    rotate(l, x, y)
 print(total)
-print(ex + 1, ey + 1)
+print(ex, ey)
