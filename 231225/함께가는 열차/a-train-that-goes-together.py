38428c90043b77a2d31cee637f6ea9192419e8bb
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-25 23:21:04 +0900

    Title: 함께가는 열차, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231225/함께가는 열차/a-train-that-goes-together.py
+++ 231225/함께가는 열차/a-train-that-goes-together.py
@@ -4,6 +4,7 @@ result = 0
 for _ in range(N):
     loc, speed = map(int, input().split())
     trains.append((loc, speed))
+trains.sort(key=lambda x:(x[1]))
 min_speed = int(1e9) + 1
 for l, s in trains:
     if min_speed >= s:
