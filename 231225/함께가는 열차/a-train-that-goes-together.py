0e048e66b329bf8531d1709fbe6ac243a9e5aa5b
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-25 23:27:12 +0900

    Title: 함께가는 열차, Time: 298ms, Memory: 30MB, Status: Passed - CodeTree

--- 231225/함께가는 열차/a-train-that-goes-together.py
+++ 231225/함께가는 열차/a-train-that-goes-together.py
@@ -6,6 +6,7 @@ for _ in range(N):
     trains.append((loc, speed))
 
 min_speed = int(1e9) + 1
+# 출발점이 뒤에있는 열차 속도가 앞 열차보다 빠른지 판단(뒤집어준다)
 for l, s in trains[::-1]:
     if min_speed >= s:
         min_speed = s
