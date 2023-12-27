e6b7d1f9bf431f7cdded889fb5335a49de70e9c0
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-27 21:54:16 +0900

    Title: 오름차순으로 정렬되는 수열, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 231227/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
+++ 231227/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
@@ -2,21 +2,9 @@ N = int(input())
 numbers = []
 for _ in range(N):
     numbers.append(int(input()))
-min_val = numbers[-1]
-min_idx = N - 1
 target = sorted(numbers)
 result = 0
-for i in range(N - 1, -1, -1):
-    if numbers[i] > min_val:
-        if min_idx + 1 < N and numbers[i] == numbers[min_idx + 1]:
-            continue
-        numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
-        # print(numbers, min_val, min_idx)
-        min_val = numbers[i]
-        min_idx = i
+for i in range(N):
+    if target[i] != numbers[i]:
         result += 1
-    # elif numbers[i] < min_val:
-    #     min_val = numbers[i]
-    #     min_idx = i
-    #     result += 1
-print(result)
+print(result - 1)
