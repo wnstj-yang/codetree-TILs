fbabc7121161538648cca288c035ce0c6e726514
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-27 21:54:44 +0900

    Title: 오름차순으로 정렬되는 수열, Time: 181ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
+++ 231227/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
@@ -4,6 +4,7 @@ for _ in range(N):
     numbers.append(int(input()))
 target = sorted(numbers)
 result = 0
+# 변경된 갯수를 카운팅하지만 횟수를 구하므로 마지막에 갯수 - 1 = 횟수로 표시
 for i in range(N):
     if target[i] != numbers[i]:
         result += 1
