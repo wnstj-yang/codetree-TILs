e3c6d5b6edab746aaa687b1114d48adf5a860d3c
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-28 21:37:05 +0900

    Title: 3개씩 묶기, Time: 160ms, Memory: 31MB, Status: Passed - CodeTree

--- 231228/3개씩 묶기/bundle-by-3.py
+++ 231228/3개씩 묶기/bundle-by-3.py
@@ -1,11 +1,13 @@
 N = int(input())
 numbers = list(map(int, input().split()))
-numbers.sort()
+numbers.sort(reverse=True)
 result = 0
 for i in range(0, N, 3):
     num = numbers[i:i+3]
+    # print(num)
     if len(num) == 3:
-        result += sum(num[1:])
+        num.pop(num.index(min(num)))
+        result += sum(num)
     else:
         result += sum(num)
 print(result)
