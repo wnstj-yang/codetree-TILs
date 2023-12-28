fb06b63e48fbf7b078220bcbd652da6efe6e7fb1
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-28 15:37:56 +0900

    Title: 수의 곱셈, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231228/수의 곱셈/multiplication-of-numbers.py
@@ -0,0 +1,11 @@
+numbers = list(map(int, input().split()))
+odds = []
+evens = []
+for i in range(2):
+    for j in range(i + 1, 3):
+        number = numbers[i] * numbers[j]
+        if number not in numbers:
+            numbers.append(number)
+numbers.append(numbers[0] * numbers[1] * numbers[2])
+numbers.sort(reverse=True)
+print(numbers[0])
