e2b6ee37d02b5dad6e0c31dab9ba05986726c8d6
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2023-12-31 15:02:25 +0900

    Title: 가격 예측, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231231/가격 예측/price-prediction.py
+++ 231231/가격 예측/price-prediction.py
@@ -5,13 +5,23 @@ for _ in range(n):
 buy, sell = prices[0], prices[0]
 if n > 1:
     for i in range(1, n):
-        if buy > prices[i]:
-            buy = prices[i]
 
         if sell < prices[i] or i == n - 1:
             sell = prices[i]
+            if i == n -1:
+                stocks = w // buy
+                left = w % buy
+                w = (stocks * sell) + left
+                buy = prices[i]
+        else:
             stocks = w // buy
             left = w % buy
             w = (stocks * sell) + left
+
             buy = prices[i]
+            sell = prices[i]
+
+        if buy > prices[i]:
+            buy = prices[i]
+        
 print(w)
