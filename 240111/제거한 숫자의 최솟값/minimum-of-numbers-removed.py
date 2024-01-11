09d4d01b5f9ba17a012c013286796cb35134b278
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2024-01-11 23:41:21 +0900

    Title: 제거한 숫자의 최솟값, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240111/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
+++ 240111/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
@@ -4,4 +4,9 @@ numbers.sort()
 if N < 3:
     print(numbers[-1])
 else:
-    print(numbers[0] + numbers[-1])
+    result = numbers[0] + numbers[-1]
+    for num in numbers[::-2]:
+        if num + numbers[0] == numbers[-1]:
+            result = num + numbers[0]
+            break
+    print(result)
