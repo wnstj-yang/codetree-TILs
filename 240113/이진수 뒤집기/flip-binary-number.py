9f7332fdd9f199222c010c7fb6401862ecf731c8
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2024-01-13 21:02:39 +0900

    Title: 이진수 뒤집기, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/이진수 뒤집기/flip-binary-number.py
+++ 240113/이진수 뒤집기/flip-binary-number.py
@@ -5,4 +5,8 @@ cnt = 0
 for i in range(len(first)):
     if first[i] != second[i]:
         cnt += 1
+# 규칙 상으로는 1의 개수를 각각 구하고 뺀 값이 서로 교환하는 조건
+# 하나씩 바꾸는 것이 다른 조건
+# 이 둘을 합한 것의 2로 나눈 것이 답
+# 규칙은 이렇지만 명확한 설명은 모르겠다..
 print((cnt + abs(first_cnt - second_cnt)) // 2)
