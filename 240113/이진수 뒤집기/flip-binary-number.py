d927ab811249aa07d85f296e4cfa07089760975a
wnstj-yang <77601720+wnstj-yang@users.noreply.github.com>
2024-01-13 17:13:02 +0900

    Title: 이진수 뒤집기, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/이진수 뒤집기/flip-binary-number.py
+++ 240113/이진수 뒤집기/flip-binary-number.py
@@ -1,4 +1,8 @@
 first, second = list(map(str, input().split()))
 first_cnt = first.count('1')
 second_cnt = second.count('1')
-print(abs(first_cnt - second_cnt) + 1)
+cnt = 0
+for i in range(len(first)):
+    if first[i] != second[i]:
+        cnt += 1
+print((cnt + abs(first_cnt - second_cnt)) // 2)
