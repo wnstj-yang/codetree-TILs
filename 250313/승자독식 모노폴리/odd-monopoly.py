44ebadd030399405be260c7f13c706151042685d
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-03-13 20:47:19 +0900

    Title: 승자독식 모노폴리, Time: 244ms, Memory: 24MB, Status: Passed - CodeTree

--- 250313/승자독식 모노폴리/odd-monopoly.py
+++ 250313/승자독식 모노폴리/odd-monopoly.py
@@ -8,11 +8,8 @@ def move():
     next_board = [item[:] for item in board]
     next_players = {}
     for num, value in players.items():
-        # print(num ,value)
         x, y = value
         d, cnt = board[x][y][1], board[x][y][2]
-        # 이거 왜 다른지 확인해야함 !
-        # print(key, board[x][y])
         is_moved = False
         for nd in players_directions[num][d]:
             nx = x + dx[nd]
@@ -23,6 +20,7 @@ def move():
                     if len(next_board[nx][ny]) > 0 and next_board[nx][ny][0] < num:
                         is_moved = True
                         break
+                    # 움직이려할 때 존재하는 경우에 기존에 있는 값을 통해 player의 위치를 초기화해준다
                     if len(next_board[nx][ny]) > 0:
                         prev_num = next_board[nx][ny][0]
                         del next_players[prev_num]
@@ -33,7 +31,7 @@ def move():
                     break
 
         if not is_moved:
-            # print(players_directions)
+
             for nd in players_directions[num][d]:
                 nx = x + dx[nd]
                 ny = y + dy[nd]
@@ -42,8 +40,7 @@ def move():
                     next_board[nx][ny] = [num, nd, K]
                     new_occupy[nx][ny] = True
                     break
-    # for num in remove_players:
-    #     del players[num]
+
     return next_players, next_board
 
 
@@ -80,7 +77,6 @@ for i in range(M):
         players_directions[i + 1][j + 1] = list(map(int, input().split()))
 
 turn = 1
-# print(players)
 while turn <= 1000:
     new_occupy = [[False] * N for _ in range(N)]
     players, board = move()
@@ -89,10 +85,6 @@ while turn <= 1000:
         keys = list(players.keys())
         if keys[0] == 1:
             break
-    # print(turn)
-    # for z in board:
-    #     print(z)
-    # print()
     turn += 1
 if turn >= 1001:
     print(-1)
